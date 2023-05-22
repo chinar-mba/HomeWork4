f417e5fb24d8433041412d201744f5542d723ae5
chinar-mba <chinar.mba@gmail.com>
2023-05-22 02:58:25 -0400

    Initial Commit

---
+++ HomeWork3/HomeWork3App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork3App.swift
+//  HomeWork3
+//
+//  Created by Chinara on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
